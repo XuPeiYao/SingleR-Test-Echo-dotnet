62fac0e22b4df44648473b1c2aebd0bc91a805b9
XuPeiYao <st89382000@gmail.com>
2018-04-29 23:29:16 +0800

    EchoHub

---
+++ SingleR_Test/Hubs/EchoHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingleR_Test.Hubs {
+    public class EchoHub : Hub {
+        public async Task SendMessage(string message) {
+            // 調用所有客戶端的ReceiveMessage方法，並帶入message參數
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
--- SingleR_Test/Startup.cs
+++ SingleR_Test/Startup.cs
@@ -8,33 +8,41 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SingleR_Test.Hubs;
 
-namespace SingleR_Test
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace SingleR_Test {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddCors();
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseStaticFiles();
+
+            app.UseCors(c => {
+                c.AllowAnyOrigin();
+                c.AllowCredentials();
+                c.AllowAnyMethod();
+                c.AllowAnyHeader();
+            });
+
+            app.UseSignalR(routes => {
+                routes.MapHub<EchoHub>("/echo");
+            });
         }
     }
 }
